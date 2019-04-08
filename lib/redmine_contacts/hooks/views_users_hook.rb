d7183fee285a251e2d512144782f7754e84db6de
James <james@click-ap.com>
2019-04-08 18:02:23 +0800

    modify hooks bug

--- lib/redmine_contacts/hooks/views_users_hook.rb
+++ lib/redmine_contacts/hooks/views_users_hook.rb
@@ -22,7 +22,7 @@
 module RedmineContacts
   module Hooks
     class ViewsUsersHook < Redmine::Hook::ViewListener
-      render_on :view_account_left_bottom, :partial => "users/contact", :locals => {:user => @user}
+      #render_on :view_account_left_bottom, :partial => "users/contact", :locals => {:user => @user}
     end
   end
 end
